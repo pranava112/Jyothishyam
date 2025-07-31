c8819ae28d8c32294cddc88e1c3878f74f475b12
pranava112 <pranavamanjunath@gmail.com>
2025-07-31 22:50:41 +0530

    vercel config

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,16 +1,41 @@
 import AllStotras from "./Stotras/AllStotras";
 import ErrorPage from "./logs/ErrorPage";
-import HanumanChalisa from "./Stotras/HanumanChalisa";
 import Home from "./logs/Home";
-import Kanakadhara from "./Stotras/KanakadharaStotram";
-import KanakadharaStotram from "./Stotras/KanakadharaStotram";
 import Layout from "./logs/Layout";
 import Map from "./logs/Map";
 import PlanetaryPositions from "./PlanetaryPositions"
 import React from "react";
 import UserDashBoard from "./logs/UserDashBoard";
-import VishnusahasranamaStotram from "./Stotras/VishnusahasranamaStotram";
 import { createBrowserRouter } from "react-router-dom";
+
+// import KanakadharaStotram from "./Stotras/KanakadharaStotram";
+
+
+
+
+
+
+
+// import Kanakadhara from "./Stotras/KanakadharaStotram";
+
+
+
+
+
+
+
+
+// import HanumanChalisa from "./Stotras/HanumanChalisa";
+
+
+
+
+
+
+
+
+// import VishnusahasranamaStotram from "./Stotras/VishnusahasranamaStotram";
+
 
 export const routing = createBrowserRouter([
   {
@@ -23,9 +48,9 @@ export const routing = createBrowserRouter([
       {path:"/DashBoard",element:<UserDashBoard/>},
       {path:"/PlanetaryPositions",element:<PlanetaryPositions/>},
       {path:"/AllStotras",element:<AllStotras/>},
-      {path:"/KanakaDharaStotram",element:<KanakadharaStotram/>},
-      {path:"/VishnusahasranamaStotram", element:<VishnusahasranamaStotram/>},
-      {path:"/HanumanChalisa",element:<HanumanChalisa/>},
+      // {path:"/KanakaDharaStotram",element:<KanakadharaStotram/>},
+      // {path:"/VishnusahasranamaStotram", element:<VishnusahasranamaStotram/>},
+      // {path:"/HanumanChalisa",element:<HanumanChalisa/>},
     ],
   },
 ]);
